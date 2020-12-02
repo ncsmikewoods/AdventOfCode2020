1c06aaecae2f5499dd2a6555bb48936af9cd9ee1
ncsmikewoods <mike.woods@ncontracts.com>
2020-12-01 18:30:02 -0600

    added day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new Solver();
+
+            Part1(solver);
+            Part2(solver);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(Solver solver)
+        {
+            var (a, b) = solver.Solve1();
+            Console.WriteLine($"Valid inputs: {a} and {b} and they sum up to {a + b}");
+            Console.WriteLine($"Solution 1: {a * b}");
+            Console.WriteLine("");
+        }
+
+        static void Part2(Solver solver)
+        {
+            var (a, b, c) = solver.Solve2();
+            Console.WriteLine($"Valid inputs: {a} and {b} and {c} and they sum up to {a + b + c}");
+            Console.WriteLine($"Solution 2: {a * b * c}");
+        }
+    }
+}
---
+++ Day1/Solver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    public class Solver
+    {
+        readonly List<int> _inputs;
+
+        public Solver()
+        {
+            _inputs = GetInputs();
+        }
+
+        public (int, int) Solve1(int goalSum = 2020)
+        {
+            foreach (var input in _inputs)
+            {
+                var diff = goalSum - input;
+                if (_inputs.Contains(diff))
+                {
+                    return (input, diff);
+                }
+            }
+
+            throw new Exception("No solution found");
+        }
+
+        public (int, int, int) Solve2()
+        {
+            foreach (var input in _inputs)
+            {
+                try
+                {
+                    var goalSum = 2020 - input;
+                    var (second, third) = Solve1(goalSum);
+
+                    return (input, second, third);
+                }
+                catch
+                {
+                }
+            }
+
+            throw new Exception("No solution found");
+        }
+
+        static List<int> GetInputs()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            Console.WriteLine($"Read {lines.Length} inputs");
+
+            return lines.Select(x => int.Parse(x.Trim())).ToList();
+        }
+    }
+}
