cee4fb436e5bd57b468d187daf66e12563fbd4a2
ncsmikewoods <mike.woods@ncontracts.com>
2020-12-04 08:29:05 -0600

    Day 4 part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new Solver();
+
+            Part1(solver);
+            Part2(solver);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(Solver solver)
+        {
+            Console.WriteLine("Solving Part 1...");
+            var start = DateTime.Now;
+
+            var validPassports = solver.Solve1();
+            var duration = DateTime.Now - start;
+
+            Console.WriteLine($"Solution 1: {validPassports}");
+            Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+            Console.WriteLine("");
+        }
+
+        static void Part2(Solver solver)
+        {
+            // Console.WriteLine("Solving Part 2...");
+            // var start = DateTime.Now;
+            //
+            // var treesHit = solver.Solve2();
+            // var duration = DateTime.Now - start;
+            //
+            // Console.Write($"Solution 2: ");
+            // Console.WriteLine(treesHit);
+            // Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+            // Console.WriteLine("");
+        }
+    }
+}
---
+++ Day4/Solver.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    public class Solver
+    {
+        static List<Passport> _passports;
+
+        public Solver()
+        {
+            GetInputs();
+        }
+
+        public int Solve1()
+        {
+            return _passports.Count(p => p.IsValid());
+        }
+
+        // public double Solve2()
+        // {
+        //     
+        // }
+
+        static void GetInputs()
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var passportsRaw = input.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Read {passportsRaw.Length} inputs");
+
+            _passports = passportsRaw.Select(ParsePassport).ToList();
+        }
+
+        static Passport ParsePassport(string passportRaw)
+        {
+            var fields = passportRaw.Replace(Environment.NewLine, " ")
+                                            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var passport = new Passport();
+
+            foreach (var field in fields)
+            {
+                var fieldTokens = field.Split(":");
+                passport.Fields.Add(fieldTokens[0], fieldTokens[1]);
+            }
+
+            return passport;
+        }
+    }
+
+    public class Passport
+    {
+        public Dictionary<string, string> Fields { get; set; } = new Dictionary<string, string>();
+
+        public bool IsValid()
+        {
+            // Console.WriteLine("Testing passport");
+            var requiredFields = new List<string>{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+
+            var missingFields = requiredFields.Except(Fields.Keys).ToList();
+
+            if (missingFields.Any())
+            {
+                // Console.WriteLine("Invalid passport.  Missing fields: " + string.Join(", ", missingFields));
+            }
+
+            return !missingFields.Any();
+        }
+    }
+}
