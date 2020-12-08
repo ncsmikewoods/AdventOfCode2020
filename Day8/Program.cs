839783332474330163de7348dfc631baed24f71f
ncsmikewoods <mike.woods@ncontracts.com>
2020-12-07 23:52:09 -0600

    day 8 part 1

---
+++ Day8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new Solver();
+
+            Part1(solver);
+            // Part2(solver);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(Solver solver)
+        {
+            Console.WriteLine("Solving Part 1...");
+            var start = DateTime.Now;
+
+            var accumulator = solver.Solve1();
+            var duration = DateTime.Now - start;
+
+            Console.WriteLine($"Solution 1: {accumulator}");
+            Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+            Console.WriteLine("");
+        }
+
+        // static void Part2(Solver solver)
+        // {
+        //     Console.WriteLine("Solving Part 2...");
+        //     var start = DateTime.Now;
+        //
+        //     var seatId = solver.Solve2();
+        //     var duration = DateTime.Now - start;
+        //     
+        //     Console.WriteLine($"Solution 2: {seatId}");
+        //     Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+        //     Console.WriteLine("");
+        // }
+    }
+}
---
+++ Day8/Solver.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day8
+{
+    public class Solver
+    {
+        static LineOfCode[] _inputs;
+
+        public Solver()
+        {
+            GetInputs();
+        }
+
+        public int Solve1()
+        {
+            var executedLines = new List<int>();
+            var cursorPosition = 0;
+            var accumulator = 0;
+
+            while (true)
+            {
+                if (executedLines.Contains(cursorPosition)) break;
+                executedLines.Add(cursorPosition);
+
+                var lineOfCode = _inputs[cursorPosition];
+                switch (lineOfCode.Instruction)
+                {
+                    case "nop": 
+                        cursorPosition++;
+                        break;
+
+                    case "acc":
+                        accumulator += lineOfCode.Value;
+                        cursorPosition++;
+                        break;
+
+                    case "jmp":
+                        cursorPosition += lineOfCode.Value;
+                        break;
+                }
+            }
+
+            return accumulator;
+        }
+
+        // public int Solve2()
+        // {
+        //     var seatIds = Solve1();
+        //     seatIds.Sort();
+        //
+        //     var validSeats = Enumerable.Range(seatIds[0], seatIds.Count);
+        //     var missingSeats = validSeats.Except(seatIds);
+        //
+        //     return missingSeats.First();
+        // }
+
+        static void GetInputs()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            Console.WriteLine($"Read {lines.Length} inputs");
+
+            _inputs = new LineOfCode[lines.Length];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var tokens = line.Split(new[] { " ", "+" }, StringSplitOptions.RemoveEmptyEntries);
+
+                _inputs[i] = new LineOfCode
+                {
+                    LineNumber = i,
+                    Instruction = tokens[0],
+                    Value = int.Parse(tokens[1])
+                };
+            }
+        }
+    }
+
+    public class LineOfCode
+    {
+        public int LineNumber { get; set; } 
+        public string Instruction { get; set; }
+        public int Value { get; set; }
+    }
+}
