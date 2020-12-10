2424de61c9fc8b3aafb1a1008cb4394e26681f5a
ncsmikewoods <mike.woods@ncontracts.com>
2020-12-09 23:52:41 -0600

    Day 10 part 1

---
+++ Day10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solver = new Solver();
+
+            Part1(solver);
+            // Part2(solver);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(Solver solver)
+        {
+            Console.WriteLine("Solving Part 1...");
+            var start = DateTime.Now;
+
+            var solution = solver.Solve1();
+            var duration = DateTime.Now - start;
+
+            Console.WriteLine($"Solution 1: {solution}");
+            Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+            Console.WriteLine("");
+        }
+
+        // static void Part2(Solver solver)
+        // {
+        //     Console.WriteLine("Solving Part 2...");
+        //     var start = DateTime.Now;
+        //
+        //     var encryptionKey = solver.Solve2();
+        //     var duration = DateTime.Now - start;
+        //     
+        //     Console.WriteLine($"Solution 2: {encryptionKey}");
+        //     Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+        //     Console.WriteLine("");
+        // }
+    }
+}
---
+++ Day10/Solver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day10
+{
+    public class Solver
+    {
+        static List<int> _adapters;
+
+        public Solver()
+        {
+            GetInputs();
+        }
+
+        public int Solve1()
+        {
+            var distribution = new Dictionary<int, int>{{1, 0}, { 2, 0 } , { 3, 0 }};
+
+            distribution[_adapters[0]]++;
+
+            for (var i = 0; i < _adapters.Count - 1; i++)
+            {
+                var diff = _adapters[i + 1] - _adapters[i];
+                distribution[diff]++;
+            }
+
+            distribution[3]++;
+
+            return distribution[1] * distribution[3];
+        }
+
+        // public double Solve2()
+        // {
+        //     
+        // }
+
+        static void GetInputs()
+        {
+            var lines = File.ReadAllLines("input.txt");
+            Console.WriteLine($"Read {lines.Length} inputs");
+
+            _adapters = lines.Select(int.Parse).ToList();
+            _adapters.Sort();
+        }
+    }
+}
